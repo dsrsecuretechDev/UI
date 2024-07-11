386709f4d4aabb5a0de25495b5efcb025515db1a
Siranjivipandi-S <siranjivipandi@gmail.com>
2024-07-11 17:09:03 +0530

    Adding Styles

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import dsrLogo from "../assets/dsrLogo.jpeg";
+import profile from "../assets/profile.jpg";
 import {
   FaArrowCircleLeft,
   FaChevronDown,
@@ -42,16 +43,19 @@ function Sidebar() {
             }`}
           />
           <h1
-            className={`text-purple-500 origin-left font-medium text-xl duration-200 ${
+            className={`text-purple-500 select-none origin-left font-medium text-xl duration-200 ${
               !open && "scale-0"
             }`}
           >
             DSR Secure Tech
           </h1>
         </div>
+        <br />
+        <div className="h-[1px] w-full bg-slate-500 rounded-lg"></div>
         <ul className="flex pt-10 flex-col gap-5">
           <li>
             <Link
+              title="Dashboard"
               to={"/"}
               className="flex rounded-md p-2 shadow-sm cursor-pointer bg-purple-300 hover:text-white text-gray-300 text-sm items-center gap-x-4 select-none"
             >
@@ -65,9 +69,9 @@ function Sidebar() {
               </span>
             </Link>
           </li>
-          <li>
+          <li className="relative group">
             <div
-              className="flex rounded-md p-2  cursor-pointer hover:bg-purple-300 hover:text-white text-gray-300 text-sm items-center gap-x-4 select-none"
+              className="flex rounded-md p-2 cursor-pointer hover:bg-purple-300 hover:text-white text-gray-300 text-sm items-center gap-x-4 select-none"
               onClick={handleDropdown}
             >
               <FaUser size={20} color="black" />
@@ -107,21 +111,60 @@ function Sidebar() {
                 </li>
               </ul>
             )}
-          </li>
-          <li>
-            <Link
-              to={"/logout"}
-              className={`flex rounded-md p-2 cursor-pointer bg-red-500 text-gray-300 text-sm hover:bg-red-300 hover:text-red-500 items-center origin-left gap-x-4 ${
-                open ? "mt-auto" : "mt-[420px]"
-              }`}
-            >
-              <IoMdLogOut size={20} />
-              <span className={`${open ? "text-xl text-slate-200" : "hidden"}`}>
-                Log Out
-              </span>
-            </Link>
+            {!open && (
+              <div className="absolute left-20 top-0 hidden group-hover:block bg-slate-50 p-2 rounded-md shadow-lg z-10">
+                <ul>
+                  <li>
+                    <Link
+                      to={"/addProfile"}
+                      className="flex items-center gap-2 p-2 hover:bg-purple-400 rounded-md select-none"
+                    >
+                      <IoMdAddCircle size={20} />
+                      <span>Add Profile</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to={"/listProfile"}
+                      className="flex items-center gap-2 p-2 hover:bg-purple-400 rounded-md select-none"
+                    >
+                      <CiCircleList size={20} />
+                      <span>List Profile</span>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            )}
           </li>
         </ul>
+        <div
+          className={`absolute bottom-5 left-5  h-auto flex items-center justify-between gap-x-4 ${
+            open ? "bg-slate-200 w-[250px]" : "bg-red-500"
+          } transition-colors shadow-md  p-1 rounded-lg`}
+        >
+          {open && (
+            <div className="flex items-center gap-4">
+              <img
+                src={profile}
+                alt="User"
+                className="h-8 w-8 rounded-full select-none"
+              />
+              <span className="text-lg select-none">User Name</span>
+            </div>
+          )}
+
+          <Link
+            title="Log Out"
+            to={"/logout"}
+            className="flex items-center p-2 cursor-pointer text-gray-300 text-sm rounded-full"
+          >
+            <IoMdLogOut
+              size={22}
+              color={open ? "gray" : "white"}
+              className="hover:text-red-500"
+            />
+          </Link>
+        </div>
       </div>
     </div>
   );
